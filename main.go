c846dc45c05f4ad075dee9437fcbeba746b88a7d
Thanh Nguyen <thanhnd@uchicago.edu>
2017-11-30 21:42:01 -0600

    fix(main): add modifications in main.go

--- main.go
+++ main.go
@@ -16,8 +16,11 @@ limitations under the License.
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -28,6 +31,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/google/go-github/github"
+	"github.com/pepper/mygithub"
 )
 
 const (
@@ -40,12 +44,13 @@ const (
 var (
 	token  string
 	enturl string
-	orgs   stringSlice
+	org    string
 	nouser bool
 	dryrun bool
 
-	debug   bool
-	version bool
+	debug      bool
+	version    bool
+	exceptions map[string]stringSlice
 )
 
 // stringSlice is a slice of strings
@@ -64,7 +69,7 @@ func init() {
 	// parse flags
 	flag.StringVar(&token, "token", "", "GitHub API token")
 	flag.StringVar(&enturl, "url", "", "GitHub Enterprise URL")
-	flag.Var(&orgs, "orgs", "organizations to include")
+	flag.StringVar(&org, "org", "", "organization to include")
 	flag.BoolVar(&nouser, "nouser", false, "do not include your user")
 	flag.BoolVar(&dryrun, "dry-run", false, "do not change branch settings just print the changes that would occur")
 
@@ -93,10 +98,22 @@ func init() {
 		usageAndExit("GitHub token cannot be empty.", 1)
 	}
 
-	if nouser && orgs == nil {
+	if nouser && org == "" {
 		usageAndExit("no organizations provided", 1)
 	}
+	file, e := ioutil.ReadFile("./exception-repos.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	e = json.Unmarshal(file, &exceptions)
+	if e != nil {
+		fmt.Printf("Json error: %v\n", e)
+		os.Exit(1)
+	}
 }
+
+type fn func(context.Context, *github.Client, *mygithub.MyClient, string, int, int) (int, error)
 
 func main() {
 	// On ^C, or SIGTERM handle exit.
@@ -128,86 +145,128 @@ func main() {
 
 	if !nouser {
 		// Get the current user
-		user, _, err := client.Users.Get("")
+		user, _, err := client.Users.Get(context.Background(), "")
 		if err != nil {
 			logrus.Fatal(err)
 		}
-		username := *user.Login
+		updateRepositories(client, *user.Login, getRepositories)
 		// add the current user to orgs
-		orgs = append(orgs, username)
-	}
-
-	page := 1
-	perPage := 20
-	if err := getRepositories(client, page, perPage); err != nil {
-		logrus.Fatal(err)
+	} else {
+		updateRepositories(client, org, getRepositoriesByOrg)
 	}
 }
 
-func getRepositories(client *github.Client, page, perPage int) error {
+func updateRepositories(client *github.Client, subjectName string, function fn) {
+	page := 1
+	perPage := 20
+	myClient := mygithub.NewClient(client)
+	curPage, err := function(context.Background(), client, myClient, subjectName, page, perPage)
+	for curPage != -1 && err == nil {
+		curPage, err = function(context.Background(), client, myClient, subjectName, curPage, perPage)
+	}
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+}
+
+func getRepositories(ctx context.Context, client *github.Client, myClient *mygithub.MyClient, user string, page int, perPage int) (int, error) {
 	opt := &github.RepositoryListOptions{
 		ListOptions: github.ListOptions{
 			Page:    page,
 			PerPage: perPage,
 		},
 	}
-	repos, resp, err := client.Repositories.List("", opt)
+	repos, resp, err := client.Repositories.List(ctx, user, opt)
 	if err != nil {
-		return err
+		return -1, err
 	}
+	return handleRepoAndNext(ctx, client, myClient, user, repos, page, resp)
+}
 
+func getRepositoriesByOrg(ctx context.Context, client *github.Client, myClient *mygithub.MyClient, org string, page int, perPage int) (int, error) {
+	fmt.Println("By org")
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{
+			Page:    page,
+			PerPage: perPage,
+		},
+	}
+	repos, resp, err := client.Repositories.ListByOrg(ctx, org, opt)
+	if err != nil {
+		return -1, err
+	}
+	return handleRepoAndNext(ctx, client, myClient, org, repos, page, resp)
+}
+
+func handleRepoAndNext(ctx context.Context, client *github.Client, myClient *mygithub.MyClient, subject string, repos []*github.Repository, page int, resp *github.Response) (int, error) {
 	for _, repo := range repos {
-		if err := handleRepo(client, repo); err != nil {
+		if subject != *repo.Owner.Login || in(exceptions["exceptions"], *repo.FullName) {
+			continue
+		}
+		if err := handleRepo(ctx, client, myClient, repo); err != nil {
 			logrus.Warn(err)
 		}
 	}
 
 	// Return early if we are on the last page.
 	if page == resp.LastPage || resp.NextPage == 0 {
-		return nil
+		return -1, nil
 	}
 
-	page = resp.NextPage
-	return getRepositories(client, page, perPage)
+	return resp.NextPage, nil
 }
 
 // handleRepo will return nil error if the user does not have access to something.
-func handleRepo(client *github.Client, repo *github.Repository) error {
-	opt := &github.ListOptions{
-		PerPage: 100,
-	}
-
-	branches, resp, err := client.Repositories.ListBranches(*repo.Owner.Login, *repo.Name, opt)
+func handleRepo(ctx context.Context, client *github.Client, myClient *mygithub.MyClient, repo *github.Repository) error {
+	fmt.Println(*repo.FullName)
+	fmt.Println(*repo.DefaultBranch)
+	branch, resp, err := client.Repositories.GetBranch(ctx, *repo.Owner.Login, *repo.Name, *repo.DefaultBranch)
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
 		return nil
 	}
 	if err != nil {
 		return err
 	}
+	return handleBranch(ctx, client, myClient, repo, branch)
+}
 
-	for _, branch := range branches {
-		if *branch.Name == "master" && in(orgs, *repo.Owner.Login) {
-			// return early if it is already protected
-			if *branch.Protection.Enabled {
-				fmt.Printf("[OK] %s:%s is already protected\n", *repo.FullName, *branch.Name)
-				return nil
-			}
-
-			fmt.Printf("[UPDATE] %s:%s will be changed to protected\n", *repo.FullName, *branch.Name)
-			if dryrun {
-				// return early
-				return nil
-			}
-
-			// set the branch to be protected
-			b := true
-			branch.Protection.Enabled = &b
-			if _, _, err := client.Repositories.EditBranch(*repo.Owner.Login, *repo.Name, *branch.Name, branch); err != nil {
-				return err
-			}
-		}
+func handleBranch(ctx context.Context, client *github.Client, myClient *mygithub.MyClient, repo *github.Repository, branch *github.Branch) error {
+	protectionRequest := &mygithub.CdisProtectionRequest{
+		RequiredStatusChecks: nil,
+		// &github.RequiredStatusChecks{
+		// 	Strict:   true,
+		// 	Contexts: []string{"continuous-integration/travis-ci", "codacy/pr"},
+		// },
+		RequiredPullRequestReviews: &mygithub.PullRequestReviewsEnforcementRequest{
+			DismissStaleReviews:     false,
+			RequireCodeOwnerReviews: true,
+		},
+		EnforceAdmins: true,
+		// TODO: Only organization repositories can have users and team restrictions.
+		//       In order to be able to test these Restrictions, need to add support
+		//       for creating temporary organization repositories.
+		Restrictions: nil,
 	}
 
+	if branch.Protected != nil && *branch.Protected {
+		fmt.Printf("[OK] %s:%s is already protected\n", *repo.FullName, *branch.Name)
+		return nil
+	}
+
+	fmt.Printf("[UPDATE] %s:%s will be changed to protected\n", *repo.FullName, *branch.Name)
+	if dryrun {
+		// return early
+		return nil
+	}
+
+	// set the branch to be protected
+	b := true
+	branch.Protected = &b
+	if _, _, err := myClient.Repositories.UpdateBranchProtection(ctx, *repo.Owner.Login, *repo.Name, *branch.Name, protectionRequest); err != nil {
+		return err
+	}
+	fmt.Printf("[UPDATE] %s:%s has been changed to protected\n", *repo.FullName, *branch.Name)
 	return nil
 }
 
